d50d6871761dbb4a8d2e1aee85399c7f08fad513
BuildFrameDigitalSystem <buildframe.digitalsystem@gmail.com>
2026-05-13 09:55:24 +0800

    Update service worker cache for admin dashboard

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,33 @@
-const CACHE_NAME = "mn-buildframe-os-v2";
+const CACHE_NAME = "mn-buildframe-os-v3";
 
 const urlsToCache = [
     "./",
     "./index.html",
+
     "./admin.html",
+    "./admin-dashboard.html",
     "./master-dashboard.html",
+
+    "./cart.html",
+    "./customer-login.html",
+    "./customer-signup.html",
+    "./customer-dashboard.html",
 
     "./manifest-store.json",
     "./manifest-admin.json",
 
     "./assets/css/store.css",
+    "./assets/css/base.css",
+    "./assets/css/admin-layout.css",
     "./assets/css/admin-dashboard.css",
+    "./assets/css/admin-forms.css",
     "./assets/css/master-dashboard.css",
 
+    "./assets/js/api-config.js",
     "./assets/js/store.js",
     "./assets/js/admin-main.js",
+    "./assets/js/admin-dashboard.js",
+    "./assets/js/permission.js",
 
     "./assets/images/branding/mn-banner.png",
     "./assets/images/branding/mn-logo.png"
